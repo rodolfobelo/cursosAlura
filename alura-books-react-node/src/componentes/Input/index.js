a888f2306fdf4592cd9d202b50428da48a50e177
-g <-g>
2024-06-16 15:49:37 -0300

    update

--- alura-books-react-node/src/componentes/Input/index.js
+++ alura-books-react-node/src/componentes/Input/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Input = styled.input `
     order: 1px solid #FFF;
-            background: transparent;
+        //     background: transparent;
             border: 1px solid #FFF;
             padding: 20px 140px;
             border-radius: 50px;
